67829a056732396be810e76f18e2dfcb7831e159
Mateus <mateuferrei@hotmail.com>
2021-05-04 21:08:40 -0300

    criando projeto

---
+++ src/main/java/com/drummond/sokovia/SokoviaApplication.java
@@ -0,0 +1,13 @@
+package com.drummond.sokovia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SokoviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SokoviaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drummond/sokovia/SokoviaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drummond.sokovia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SokoviaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
